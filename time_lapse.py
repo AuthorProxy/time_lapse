c3b4f2e15287f674d93d65660a370d0014c838ea
anatolyburtsev <anatolyburtsev@yandex.ru>
2013-11-26 09:52:55 +0400

    devide to some functions

--- time_lapse.py
+++ time_lapse.py
@@ -2,7 +2,7 @@ __author__ = 'onotole'
 import os,re,random,shutil,time
 
 pathSD="/Volumes/NO NAME"
-delayBefore=600 #10 min
+delayBefore=600  #10 min
 tmpDir="/tmp/.timelapse" + str(random.randint(0,100)) + "/"
 dstPath="~/Desktop/"
 
@@ -13,37 +13,52 @@ def normalize(number, measure):
     result += str(number)
     return result
 
+def createFileList(path=pathSD):
+    path_f=[]
+    for d, dirs, files in os.walk(path):
+        for f in files:
+            if f.endswith('jpg') or f.endswith('JPG'):
+                path = os.path.join (d,f)
+                path_f.append(path)
+    path_f.sort()
+    return path_f
+
+def findTimeLapserRaw(path_f):
+    oldCreateTime=0.
+    for file in path_f:
+        createTime = os.path.getctime(file)
+        if oldCreateTime > 0 and createTime - oldCreateTime > delayBefore:
+            startTimeLapse=file
+        if oldCreateTime > 0 and oldCreateTime - createTime > delayBefore:
+            pass #print (createTime, oldCreateTime, file, "smth get wrong")
+        oldCreateTime = createTime
+
+    filesToTimeLapse=path_f[path_f.index(startTimeLapse):]
+    return filesToTimeLapse
+
+def preparing(filesToTimeLapse,tmpDir=tmpDir):
+    #create tmpdir and copy images to it
+    if os.path.exists(tmpDir):
+        os.removedirs(tmpDir)
+    os.mkdir(tmpDir)
+    length = len(filesToTimeLapse)
+    for i in range(length):
+        shutil.copyfile(filesToTimeLapse[i],tmpDir + 'img-' + normalize(i,length)+".jpg")
+    zeroCount=len(str(length))
+
+def convert(tmpDir, dstPath, zeroCount):
+    convertCommand = 'cd '+ tmpDir +'; ffmpeg -f image2 -i img-%0'+ str(zeroCount) + 'd.jpg -r 24 -vcodec rawvideo -pix_fmt yuv420p -s 1280x960 '+\
+                 dstPath + 'OutputVideo' + str(time.time()).split('.')[0] + '.avi'
+    os.system(convertCommand)
+    os.system('rm -rf '+tmpDir)
+
 if __name__ == "__main__":
     pass
 
-path_f=[]
-oldCreateTime=0.
+path_f=createFileList(pathSD)
 
-for d, dirs, files in os.walk(pathSD):
-    for f in files:
-        if f.endswith('jpg') or f.endswith('JPG'):
-            path = os.path.join (d,f)
-            path_f.append(path)
-path_f.sort()
+filesToTimeLapse=findTimeLapserRaw(path_f)
 
-for file in path_f:
-    createTime = os.path.getctime(file)
-    if oldCreateTime > 0 and createTime - oldCreateTime > delayBefore:
-        startTimeLapse=file
-    if oldCreateTime > 0 and oldCreateTime - createTime > delayBefore:
-        pass #print (createTime, oldCreateTime, file, "smth get wrong")
-    oldCreateTime = createTime
+preparing(filesToTimeLapse,tmpDir)
 
-filesToTimeLapse=path_f[path_f.index(startTimeLapse):]
-if os.path.exists(tmpDir):
-    os.removedirs(tmpDir)
-os.mkdir(tmpDir)
-length=len(filesToTimeLapse)
-for i in range(length):
-    shutil.copyfile(filesToTimeLapse[i],tmpDir + 'img-' + normalize(i,length)+".jpg")
-zeroCount=len(str(length))
-convertCommand = 'cd '+ tmpDir +'; ffmpeg -f image2 -i img-%0'+ str(zeroCount) + 'd.jpg -r 24 -vcodec rawvideo -pix_fmt yuv420p -s 1280x960 '+\
-                 dstPath + 'OutputVideo' + str(time.time()).split('.')[0] + '.avi'
-print(convertCommand)
-os.system(convertCommand)
-os.system('rm -rf '+tmpDir)
+convert(tmpDir,dstPath,len(str(len(filesToTimeLapse))))
